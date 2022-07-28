4e6d1a528501046f3fcf3c02996c6e8128d997ab
young <yangyoung@esquel.com>
2022-07-28 16:42:56 +0800

    tmux

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive # Commands to run in interactive sessions can go here
 end
 
 # golang
+set -x GOPATH $HOME/go
+set -x PATH $PATH /usr/local/go/bin $GOPATH/bin
 set -x GO111MODULE on
 set -x GOPROXY https://goproxy.cn,direct
 
