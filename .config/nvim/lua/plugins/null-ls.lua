a1fdd58c8458f5e10fb1a6b835f86aeef19e0e2d
young <yangyoung@esquel.com>
2022-09-14 11:39:20 +0800

    update

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -9,22 +9,23 @@ end
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 packer.setup{
   -- you can reuse a shared lspconfig on_attach callback here
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                    vim.lsp.buf.formatting_sync()
-                end,
-            })
-        end
-    end,
-    sources = {
-        packer.builtins.formatting.stylua,
-        packer.builtins.diagnostics.eslint,
-        packer.builtins.completion.spell,
-    },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+          vim.lsp.buf.formatting_sync()
+        end,
+      })
+    end
+  end,
+  sources = {
+    packer.builtins.formatting.stylua,
+    packer.builtins.diagnostics.eslint,
+    packer.builtins.completion.spell,
+    packer.builtins.code_actions.gitsigns,
+  },
 }
