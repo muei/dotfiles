0ccded0cf356fc084d308dabbbae709fe386915f
young <yangyoung@esquel.com>
2022-09-16 10:30:26 +0800

    update

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -1,66 +1,62 @@
 -- Use a protected call so we don't error out on first use
+print("hello mason")
 local packer_name = "lspconfig"
 local status_ok, lspconfig = pcall(require, packer_name)
 if not status_ok then
-  vim.notify(packer_name .. " not found!")
-  return
+	vim.notify(packer_name .. " not found!")
+	return
 end
 
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+local opts = { noremap = true, silent = true }
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
 
 ---
 -- Global Config
 ---
 local lsp_defaults = {
-  flags = {
-    -- This is the default in Nvim 0.7+
-    debounce_text_changes = 150,
-  },
-  -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
-  capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-  -- -- Use an on_attach function to only map the following keys
-  -- -- after the language server attaches to the current buffer
-  -- on_attach = function(client, bufnr)
-  --   -- Enable completion triggered by <c-x><c-o>
-  --   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-  --
-  --   -- Mappings.
-  --   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  --   local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  --   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  --   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  --   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  --   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  --   vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  --   vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  --   vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  --   vim.keymap.set('n', '<space>wl', function()
-  --     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  --   end, bufopts)
-  --   vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  --   vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  --   vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-  --   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  --   vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
-  -- end
+	flags = {
+		-- This is the default in Nvim 0.7+
+		debounce_text_changes = 150,
+	},
+	-- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+	capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+	-- -- Use an on_attach function to only map the following keys
+	-- -- after the language server attaches to the current buffer
+	-- on_attach = function(client, bufnr)
+	--   -- Enable completion triggered by <c-x><c-o>
+	--   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	--
+	--   -- Mappings.
+	--   -- See `:help vim.lsp.*` for documentation on any of the below functions
+	--   local bufopts = { noremap=true, silent=true, buffer=bufnr }
+	--   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+	--   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+	--   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+	--   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+	--   vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+	--   vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+	--   vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+	--   vim.keymap.set('n', '<space>wl', function()
+	--     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	--   end, bufopts)
+	--   vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+	--   vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+	--   vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+	--   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+	--   vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+	-- end
 
-  on_attach = function(client, bufnr)
-    vim.api.nvim_exec_autocmds('User', {pattern = 'LspAttached'})
-  end
-
+	on_attach = function(client, bufnr)
+		vim.api.nvim_exec_autocmds("User", { pattern = "LspAttached" })
+	end,
 }
 
-lspconfig.util.default_config = vim.tbl_deep_extend(
-  'force',
-  lspconfig.util.default_config,
-  lsp_defaults
-)
+lspconfig.util.default_config = vim.tbl_deep_extend("force", lspconfig.util.default_config, lsp_defaults)
 
 ---
 -- LSP Servers
@@ -69,22 +65,21 @@ lspconfig.util.default_config = vim.tbl_deep_extend(
 -- lspconfig.sumneko_lua.setup({})
 
 require("mason").setup()
-require("mason-lspconfig").setup{
-  ensure_installed = { "sumneko_lua", "rust_analyzer" }
-}
+require("mason-lspconfig").setup({
+	ensure_installed = { "sumneko_lua", "rust_analyzer" },
+})
 
-
-lspconfig['pyright'].setup{
-  on_attach = on_attach,
-  flags = lsp_flags,
-  capabilities = capabilities,
-}
-lspconfig['rust_analyzer'].setup{
-  on_attach = on_attach,
-  flags = lsp_flags,
-  capabilities = capabilities,
-  -- Server-specific settings...
-  settings = {
-    ["rust-analyzer"] = {}
-  }
-}
+lspconfig["pyright"].setup({
+	on_attach = on_attach,
+	flags = lsp_flags,
+	capabilities = capabilities,
+})
+lspconfig["rust_analyzer"].setup({
+	on_attach = on_attach,
+	flags = lsp_flags,
+	capabilities = capabilities,
+	-- Server-specific settings...
+	settings = {
+		["rust-analyzer"] = {},
+	},
+})
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,9 +1,10 @@
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  print("Installing packer...")
-  vim.cmd [[packadd packer.nvim]]
+	packer_bootstrap =
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+	print("Installing packer...")
+	vim.cmd([[packadd packer.nvim]])
 end
 
 vim.cmd([[
@@ -17,96 +18,137 @@ vim.cmd([[
 local packer_name = "packer"
 local status_ok, packer = pcall(require, packer_name)
 if not status_ok then
-  vim.notify(packer_name .. " not found!")
-  return
+	vim.notify(packer_name .. " not found!")
+	return
 end
 
 packer.init({
-  display = {
-    -- configure Packer to use a floating window for command outputs
-    open_fn = require('packer.util').float,
-  }
+	display = {
+		-- configure Packer to use a floating window for command outputs
+		open_fn = require("packer.util").float,
+	},
 })
 
 local function config(name, path)
-  path = path or "plugins"
-  return string.format('require("%s/%s")', path, name)
+	path = path or "plugins"
+	return string.format('require("%s/%s")', path, name)
 end
 
 return packer.startup(function(use)
-  use("wbthomason/packer.nvim")
-  use("nvim-lua/plenary.nvim")
+	use("wbthomason/packer.nvim")
+	use("nvim-lua/plenary.nvim")
 
-  use {'famiu/nvim-reload'}
+	use({ "famiu/nvim-reload" })
 
-  --[[ UI ]]
-  use "kyazdani42/nvim-web-devicons" -- 字体图标
-  --[[ theme ]]
-  use "folke/tokyonight.nvim" 
-  use {"tanvirtin/monokai.nvim", config = function() require("monokai").setup{} end}
+	--[[ UI ]]
+	use("kyazdani42/nvim-web-devicons") -- 字体图标
+	--[[ theme ]]
+	use("folke/tokyonight.nvim")
+	use({
+		"tanvirtin/monokai.nvim",
+		config = function()
+			require("monokai").setup({})
+		end,
+	})
 
-  use {'kyazdani42/nvim-tree.lua', config = function() require"nvim-tree".setup() end} -- 文件目录 
-  use {'stevearc/aerial.nvim', config = function() require"aerial".setup() end} -- 大纲
-  use {"nvim-lualine/lualine.nvim", config = config"lualine"} -- 底部状态栏
-  use {'akinsho/bufferline.nvim', tag = "v2.*", config = function() require"bufferline".setup() end} -- buffer line
-  use {'lukas-reineke/indent-blankline.nvim', config = function() require"indent_blankline".setup() end} -- indent blank line
-  
-  --[[ 快捷键 ]]
-  use {"junegunn/vim-easy-align"}
-  use {'numToStr/Comment.nvim', config = function() require"Comment".setup() end} -- 注释 
-  use {'folke/which-key.nvim', config = config"which-key"} -- 快捷键映射 
-  use { 'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    config = config("telescope")
-  } -- 文件查找
+	use({
+		"kyazdani42/nvim-tree.lua",
+		config = function()
+			require("nvim-tree").setup()
+		end,
+	}) -- 文件目录
+	use({
+		"stevearc/aerial.nvim",
+		config = function()
+			require("aerial").setup()
+		end,
+	}) -- 大纲
+	use({ "nvim-lualine/lualine.nvim", config = config("lualine") }) -- 底部状态栏
+	use({
+		"akinsho/bufferline.nvim",
+		tag = "v2.*",
+		config = function()
+			require("bufferline").setup()
+		end,
+	}) -- buffer line
+	use({
+		"lukas-reineke/indent-blankline.nvim",
+		config = function()
+			require("indent_blankline").setup()
+		end,
+	}) -- indent blank line
 
-  --[[ 编辑 ]]
-  use {'kylechui/nvim-surround', config = function() require"nvim-surround".setup() end} 
-  use {"windwp/nvim-autopairs", config = function() require"nvim-autopairs".setup() end}
+	--[[ 快捷键 ]]
+	use({ "junegunn/vim-easy-align" })
+	use({
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	}) -- 注释
+	use({ "folke/which-key.nvim", config = config("which-key") }) -- 快捷键映射
+	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x", config = config("telescope") }) -- 文件查找
 
-  --[[ 语法高亮 ]]
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-  }
+	--[[ 编辑 ]]
+	use({
+		"kylechui/nvim-surround",
+		config = function()
+			require("nvim-surround").setup()
+		end,
+	})
+	use({
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup()
+		end,
+	})
 
-  --[[ lsp ]]
-  use {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-    config = config("", "lsp"),
-  }
+	--[[ 语法高亮 ]]
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			require("nvim-treesitter.install").update({ with_sync = true })
+		end,
+	})
 
-  use {'simrat39/rust-tools.nvim', config = config"rust-tools"}
+	--[[ lsp ]]
+	use({
+		"williamboman/mason.nvim",
+		requires = {
+			"williamboman/mason-lspconfig.nvim",
+			"neovim/nvim-lspconfig",
+		},
+		config = config("", "lsp"),
+	})
 
-  --[[ cmp ]]
-  use {"hrsh7th/nvim-cmp", config = config"cmp"} -- 补全
-  use "hrsh7th/cmp-path" -- 路径补全
-  use "hrsh7th/cmp-buffer" -- 当前文本编辑补全
-  use "hrsh7th/cmp-cmdline" -- 命令行补全
-  use "hrsh7th/cmp-vsnip"
-  use "hrsh7th/vim-vsnip"
-  use "hrsh7th/cmp-nvim-lsp"
-  use "hrsh7th/cmp-nvim-lua"
-  use "hrsh7th/cmp-nvim-lsp-signature-help"
-  use "f3fora/cmp-spell" -- 英文单词拼写
+	use({ "simrat39/rust-tools.nvim", config = config("rust-tools") })
 
-  --[[ format ]]
-  use {"jose-elias-alvarez/null-ls.nvim", config = config"null-ls"}
-  use "sbdchd/neoformat"
+	--[[ cmp ]]
+	use({ "hrsh7th/nvim-cmp", config = config("cmp") }) -- 补全
+	use("hrsh7th/cmp-path") -- 路径补全
+	use("hrsh7th/cmp-buffer") -- 当前文本编辑补全
+	use("hrsh7th/cmp-cmdline") -- 命令行补全
+	use("hrsh7th/cmp-vsnip")
+	use("hrsh7th/vim-vsnip")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-nvim-lua")
+	use("hrsh7th/cmp-nvim-lsp-signature-help")
+	use("f3fora/cmp-spell") -- 英文单词拼写
 
-  --[[ terminal ]]
-  use {"voldikss/vim-floaterm"}
-  use {"akinsho/toggleterm.nvim", config = config"toggleterm"}
+	--[[ format ]]
+	use({ "jose-elias-alvarez/null-ls.nvim", config = config("null-ls") })
+	use("sbdchd/neoformat")
 
+	--[[ terminal ]]
+	use({ "voldikss/vim-floaterm" })
+	use({ "akinsho/toggleterm.nvim", config = config("toggleterm") })
 
-  --[[ git ]]
-  use {"lewis6991/gitsigns.nvim", config = config"gitsigns"}
+	--[[ git ]]
+	use({ "lewis6991/gitsigns.nvim", config = config("gitsigns") })
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
