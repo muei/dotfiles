1aab9685d5c250e9464c7cefee448779d1606df7
young <yangyoung@esquel.com>
2022-10-20 09:30:17 +0800

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,13 +16,17 @@ set -gx PATH "$HOME/.local/bin" $PATH
 # Wayland
 set -x MOZ_ENABLE_WAYLAND 1
 # set -x SDL_VIDEODRIVER wayland
-#set WAYLAND_DISPLAY alacritty
+set WAYLAND_DISPLAY alacritty alacritty
 
+set _JAVA_AWT_WM_NONREPARENTING 1
 
 # Input Method
-set GTK_IM_MODULE fcitx5
-set QT_IM_MODULE fcitx5
-set XMODIFIERS @im fcitx5
+set GTK_IM_MODULE fcitx
+set QT_IM_MODULE fcitx
+set XMODIFIERS @im fcitx
+set INPUT_METHOD fcitx
+set SDL_IM_MODULE fcitx
+set GLFW_IM_MODULE ibus 
 
 
 #set -x http_proxy  http://127.0.0.1:7890
