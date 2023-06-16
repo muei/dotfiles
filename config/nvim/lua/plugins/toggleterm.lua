a040acd703a60b7f80c3a9e06e5cc54731b67510
young <yangyoung@esquel.com>
2023-06-16 16:48:18 +0800

    update

--- config/nvim/lua/plugins/toggleterm.lua
+++ config/nvim/lua/plugins/toggleterm.lua
@@ -9,6 +9,9 @@ function _G.set_terminal_keymaps()
   vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
 end
 
+if true then
+  return {}
+end
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 return {
