1bc55c19c7e0314c96c052a22a4c158f74ff6369
Young <young>
2023-11-30 14:07:52 +0800

    updated

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
--- vim.keymap.set("n", "<leader>w\\", "<C-w>v", { noremap = true, silent = true, desc = "Split window right" })
--- vim.keymap.set("n", "<leader>\\", "<C-w>v", { noremap = true, silent = true, desc = "Split window right" })
