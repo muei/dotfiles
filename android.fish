406c3cd7de6ded4a03a44840dab623bd10fe6dce
young <young>
2026-01-18 00:49:10 +0800

    update

---
+++ android.fish
@@ -0,0 +1,3 @@
+# Android SDK
+set --export ANDROID_HOME "$HOME/Android/Sdk"
+set --export PATH $ANDROID_HOME/emulator $ANDROID_HOME/platform-tools $PATH
---
+++ emulator.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+emulator -memory 2048 -cores 2 -no-audio -no-boot-anim -no-snapshot -no-snapshot-load -gpu host -accel on -avd Pixel_9_Pro
